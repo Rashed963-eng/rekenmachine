1d817a166971726507ed447d61132b07f6292efa
michael20191537 <20191537@student.hhs.nl>
2021-04-30 09:20:09 +0200

    Test voor plus functie toegevoegd

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -2,8 +2,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaFXAppTest {
+    JavaFXApp java = new JavaFXApp();
     @Test
     void aDummyTest() {
-        assertEquals(0,0);
+        assertEquals(1+1,java.computeAdd(1,1));
     }
 }
