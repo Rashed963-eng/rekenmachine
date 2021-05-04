aba420fe540ebb44733426f126d5976a9eb09aac
Rashed963-eng <80213471+Rashed963-eng@users.noreply.github.com>
2021-05-04 09:20:56 +0200

    twee klassen zijn toegevoegd

---
+++ src/main/java/MultiplyComputation.java
@@ -0,0 +1,5 @@
+public class MultiplyComputation {
+    public int compute(int number1, int number2){
+        return 0;
+    }
+}
---
+++ src/main/java/PlusComputation.java
@@ -0,0 +1,5 @@
+public class PlusComputation {
+    public int compute(int number1, int number2){
+        return 0;
+    }
+}
